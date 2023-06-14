35dc7fafda477394570fe66d38f7ee6d8f0b0198
edgarsousa21 <edgarraphaeldesousa@hotmail.com>
2023-06-14 18:45:53 -0300

    CSS Ajusting

--- script.js
+++ script.js
@@ -1,71 +1,67 @@
 
-    function calcular() {
-        let opr = window.document.getElementById('selection').value
-        let tn1 = window.document.getElementById('txtn1') //obs: são iguais de forma diferente.
-        let tn2 = window.document.querySelector('input#txtn2')
-        let res = window.document.getElementById('res')
-        let n1 = Number(tn1.value)
-        let n2 = Number(tn2.value)
+function calcular() {
+    let opr = window.document.getElementById('selection').value
+    let tn1 = window.document.getElementById('txtn1') //obs: são iguais de forma diferente.
+    let tn2 = window.document.querySelector('input#txtn2')
+    let res = window.document.getElementById('res')
+    let n1 = Number(tn1.value)
+    let n2 = Number(tn2.value)
 
-        changeOperation(opr)
-        calculatorNumb(n1, n2, opr)
-        // opr.toString()        
-        // var oper = changeOperation(opr)
-        // var s = calculatorNumb(n1, n2, oper)
-        // res.innerHTML = `A soma entre ${n1} e ${n2} é igual a: <strong>${s}</strong>`
+    changeOperation(opr)
+    calculatorNumb(n1, n2, opr)
 
+}
+function changeOperation(opr) {
+    let operation
+    if (opr === '+') { operation = '+' }
+    if (opr === '-') { operation = '-' }
+    if (opr === '*') { operation = '*' }
+    if (opr === '/') { operation = '/' }
+    if (opr === '%') { operation = '%' }
+    if (opr === '**') { operation = '**' }
+    return operation
+
+}
+
+function calculatorNumb(num1, num2, operation) {
+    let result
+    let signal
+    switch (operation) {
+        case '+':
+            result = num1 + num2
+            signal = 'A soma de '
+            break;
+
+        case '-':
+            result = num1 - num2
+            signal = 'A subtração de '
+            break;
+
+        case '*':
+            result = num1 * num2
+            signal = 'A multiplicação entre '
+            break;
+
+        case '/':
+            result = num1 / num2
+            signal = 'A Divisão entre'
+            break;
+
+        case '%':
+            result = num1 % num2
+            signal = 'O resto da divisão entre'
+            break;
+
+        case '**':
+            result = num1 ** num2
+            signal = 'O Calculo da Potência de '
+            break;
+
+        default:
+            result = num1 + num2
+            signal = 'Soma por Padrão de '
+            break;
+            return result
     }
-    function changeOperation(opr) {
-        let operation
-        if (opr === '+') { operation = '+' }
-        if (opr === '-') { operation = '-' }
-        if (opr === '*') { operation = '*' }
-        if (opr === '/') { operation = '/' }
-        if (opr === '%') { operation = '%' }
-        if (opr === '**') { operation = '**' }
-        return operation
-
-    }
-
-    function calculatorNumb(num1, num2, operation) {
-        let result
-        let signal
-        switch (operation) {
-            case '+':
-                result = num1 + num2
-                signal = 'A soma de '
-                break;
-
-            case '-':
-                result = num1 - num2
-                signal = 'A subtração de '
-                break;
-
-            case '*':
-                result = num1 * num2
-                signal = 'A multiplicação entre '
-                break;
-
-            case '/':
-                result = num1 / num2
-                signal = 'A Divisão entre'
-                break;
-
-            case '%':
-                result = num1 % num2
-                signal = 'O resto da divisão entre'
-                break;
-
-            case '**':
-                result = num1 ** num2
-                signal = 'O Calculo da Potência de '
-                break;
-
-            default:
-                result = num1 + num2
-                signal = 'Soma por Padrão de '
-                break;
-                return result
-        }
-        res.innerHTML = `${signal} ${num1} e ${num2} é igual a: <strong>${result}</strong>`
-    }
+    res.innerHTML = `${signal} ${num1} e ${num2} é igual a: <strong>${result}</strong>`
+}
